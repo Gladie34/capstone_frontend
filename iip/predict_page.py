d800f239fca9930697e8cb0faac1288ece65c7f7
Gladie34 <gladyskiprono34@gmail.com>
2024-08-24 06:01:41 +0300

    landing page

---
+++ iip/predict_page.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+
+def load_model():
+    with open('xgb_model.pkl', 'rb') as file:
+        loaded_model = pickle.load(file)
+    return loaded_model
+
+#loaded_model=load_model #execute the model
+
+def show_predict_page():
