c2f14ff43544958e9fb8346cfee6629d33cad1d1
Gladie34 <gladyskiprono34@gmail.com>
2024-08-25 14:06:46 +0300

    changes

--- iip/predict_page.py
+++ iip/predict_page.py
@@ -9,9 +9,8 @@ def load_model():
     with open('xgb_model.pkl', 'rb') as file:
         loaded_model = pickle.load(file)
     return loaded_model
-loaded_model=load_model()
 
-#loaded_model=load_model #execute the model
+loaded_model=load_model() #execute the model
 
 def show_predict_page():
     st.title("Loan prediction approval ")
