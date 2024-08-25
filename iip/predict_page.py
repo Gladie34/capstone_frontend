fec9715b21e9c8803d9e6078392092953573dee5
Gladie34 <gladyskiprono34@gmail.com>
2024-08-25 13:45:55 +0300

    data input

--- iip/predict_page.py
+++ iip/predict_page.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pickle
 import numpy as np
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
 
 
 def load_model():
@@ -15,15 +17,42 @@ def show_predict_page():
     st.write("""### we need some information to check on your loan approvals""")
 
     with st.sidebar:
-        loan_id=st.number_input("Load Id",placeholder="Type a number...")
-        no_of_dependents=st.number_input("No of dependents",placeholder="Type a number...")
+        loan_id=st.number_input("Load Id",value=None,placeholder="Type a number...")
+        no_of_dependents=st.number_input("No of dependents",value=None,placeholder="Type a number...")
         education=st.selectbox("Education Level",options=["Graduate","Not Graduate"],placeholder="Select Education level...")
         self_employed=st.selectbox("Self-Employed",options=["Yes","No"],placeholder="select...")
-        income_annum=st.number_input("Annual Income",placeholder="Type a number....")
-        loan_amount=st.number_input("Amount of Loan",placeholder="Enter Loan amount...")
-        loan_term=st.number_input("Loan Term",placeholder="Enter loan terms in months...")
-        cibil_score=st.number_input("Cibil Score",placeholder="Enter  your cibil score")
-        residential_assets_value=st.number_input("Residential Asset Value",placeholder="Enter the value of your residential asset..")
-        commercial_assets_value=st.number_input("Commercial Assets Value",placeholder="Enter your commercial asset value...")
-        luxury_assets_value=st.number_input("Luxury Assets Value",placeholder="Enter your luxury asset value...")
-        bank_assets_value=st.number_input("Bank Assets Value",placeholder="Enter your bank asset value...")
+        income_annum=st.number_input("Annual Income",value=None,placeholder="Type a number....")
+        loan_amount=st.number_input("Amount of Loan",value=None,placeholder="Enter Loan amount...")
+        loan_term=st.number_input("Loan Term",value=None,placeholder="Enter loan terms in months...")
+        cibil_score=st.number_input("Cibil Score",value=None,placeholder="Enter  your cibil score")
+        residential_assets_value=st.number_input("Residential Asset Value",value=None,placeholder="Enter the value of your residential asset..")
+        commercial_assets_value=st.number_input("Commercial Assets Value",value=None,placeholder="Enter your commercial asset value...")
+        luxury_assets_value=st.number_input("Luxury Assets Value",value=None,placeholder="Enter your luxury asset value...")
+        bank_assets_value=st.number_input("Bank Assets Value",value=None,placeholder="Enter your bank asset value...")
+
+        submit=st.buttom("Check Loan status")
+        if submit:
+            input_data = pd.DataFrame({
+                'loan_id': [loan_id],
+                'no_of_dependents': [no_of_dependents],
+                'education': [education],
+                'self_employed': [self_employed],
+                'income_annum': [income_annum],
+                'loan_amount': [loan_amount],
+                'loan_term': [loan_term],
+                'cibil_score': [cibil_score],
+                'residential_assets_value': [residential_assets_value],
+                'commercial_assets_value': [commercial_assets_value],
+                'luxury_assets_value': [luxury_assets_value],
+                'bank_assets_value': [bank_assets_value]
+            })
+            #encoding categorical variables
+            le = LabelEncoder()
+            input_data['education'] = le.fit_transform(input_data['education'])
+            input_data['self_employed'] = le.fit_transform(input_data['self_employed'])
+
+            # Prediction using the loaded model
+            prediction =load_model.predict(input_data)
+
+            # Display the prediction result
+            st.write(f"Loan Status: {'Approved' if prediction[0] == 1 else 'Rejected'}")
